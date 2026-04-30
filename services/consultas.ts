f9a0a351b3d4f443ccf76bd9a62847249dc3b869
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-04-30 15:55:11 -0300

    fix

--- services/consultas.ts
+++ services/consultas.ts
@@ -2,7 +2,7 @@ import { api } from "@/lib/api";
 import { Consulta } from "@/types/consulta";
 
 
-export async function consultaService(): Promise<Consulta> {
+export async function consultaService(): Promise<Consulta[]> {
     const res =  await api.get('papel-categoria')
     return res.data
 }

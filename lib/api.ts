02e0790901a1903164eb47b081c1f9acff7d0df4
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-04 16:41:41 -0300

    descomentando

--- lib/api.ts
+++ lib/api.ts
@@ -16,16 +16,16 @@ api.interceptors.response.use(
   }
 )
 
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
 
-//       if (typeof window !== "undefined") {
-//         window.location.href = "/login"
-//       }
-//     }
+      if (typeof window !== "undefined") {
+        window.location.href = "/login"
+      }
+    }
 
-//     return Promise.reject(error);
-//   }
-// )
+    return Promise.reject(error);
+  }
+)

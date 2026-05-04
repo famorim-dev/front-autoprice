a70c152e2f6cb9a06a73100b04f03446aafabbb6
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-04 14:58:02 -0300

    cmt

--- services/me.ts
+++ services/me.ts
@@ -4,6 +4,5 @@ import { Me } from "@/types/me";
 
 export async function me(): Promise<Me> {
     const res = await api.get('/me', {withCredentials: true})
-    console.log(res.data)
     return res.data
 }

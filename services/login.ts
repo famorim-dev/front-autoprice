49fa0980fcaa949e2e183552c07c185ad206adb0
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-05 09:40:42 -0300

    arruma cookies

--- services/login.ts
+++ services/login.ts
@@ -3,20 +3,7 @@ import { cookies } from "next/headers";
 
 
 export async function loginService(email: string, password: string): Promise<{token: string, mensagem: string}> {
-    const res = await api.post('/auth', {email, senha: password})
-        const setCookie = res.headers['set-cookie']
-
-    if (setCookie) {
-        const token = setCookie[0].split(';')[0].split('=')[1]
-        
-        const store = await cookies();
-        store.set("token", token, {
-            httpOnly: true,
-            path: "/",
-            secure: false,
-            sameSite: "lax",
-        });
-    }
+    const res = await api.post('/auth', {email, senha: password}, {withCredentials:true})
     
     return res.data
 }

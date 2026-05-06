f8b8996449c8a248b2bf0114b503d66a09f41ee8
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-06 15:44:53 -0300

    arruma cargo por pagina

--- app/consultas/page.tsx
+++ app/consultas/page.tsx
@@ -1,11 +1,24 @@
 'use client'
-
-
 import { Header } from "@/global/components/header/header"
 import TableHome from "./components/table"
+import { useEffect } from "react"
+import { redirecionaPorCargo } from "@/utils/cargo"
+import { useRouter } from "next/router"
 
 export default function Consultas() {
+  const router = useRouter()
 
+  useEffect(() => {
+    async function check() {
+      const ok = await redirecionaPorCargo(["consultor","admin"])
+
+      if (!ok) {
+        router.push("/404")
+      }
+    }
+
+    check()
+  }, [])
   return (
     <div>
       <Header/>

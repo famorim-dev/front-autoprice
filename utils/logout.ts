731c02e310da3b4172d452c00f843544ece77012
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-13 11:12:01 -0300

    arruma remove cookies

--- utils/logout.ts
+++ utils/logout.ts
@@ -1,7 +1,6 @@
 'use server'
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
-import path from "path"
 
 export async function logout(){
         const store = await cookies()

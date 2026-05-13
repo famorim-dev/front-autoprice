e8ab4b0f79c9f7f334f56e4cbd1db4ff7db3f9a9
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-05-13 11:05:36 -0300

    arruma delete cookies

--- utils/logout.ts
+++ utils/logout.ts
@@ -1,9 +1,10 @@
 'use server'
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import path from "path"
 
 export async function logout(){
         const store = await cookies()
-        store.delete("token")
+        store.delete({name: "token", path: '/' })
         redirect("/login")
 }
